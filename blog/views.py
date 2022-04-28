1f54850c417c8cccd79475b266ff3cbfaa8cae10
Hamed poorgholam <redminoteh8@gmail.com>
2022-04-28 14:54:59 +0430

    initializing for runinig server

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
+import imp
+import django
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def test(request):
+
+    return HttpResponse("<h1> this is a test </h2>")
